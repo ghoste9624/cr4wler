7d0e66fd22661df33a25f8ebaa7c262d2f216ff2
ghoste9624 <jackspade44@outlook.com>
2025-05-31 07:04:42 -0400

    Create cr4wler.sh

---
+++ cr4wler.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "\e[92m
+ + -- --=[ \e[1;94msimple.osint.builds v1.1.2    \e[92m]
+ + -- --=[ \e[1;94mhttps://github.com/ghoste9624 \e[92m]
+ + -- --=[ \e[1;94mcr4wler \e[92m> \e[1;94mCrawling Scripts    \e[92m]\e[1;97m"
+      echo ""
+
+echo -e "\e[0;37m"
+echo " Usage Options"
+echo ""                                                                                        
+echo "    python3 cr4wler1.py"
+echo ""
+echo "    python3 cr4wler2.py <url>"
+echo ""
+echo "    python3 cr4wler3.py <script>"
+echo ""
+echo ""
